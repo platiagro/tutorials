fc11f139b9669bd775efaeb992ff5ff0881eb279
fberanizo <fabio.beranizo@gmail.com>
2020-06-13 02:46:57 +0000

    Deploying to gh-pages from  @ 4df283c3dec5d3c6e0671c53847602fced7c1044 🚀

---
+++ machine-failure/precache-manifest.ad28c52b4114a80b375d2eab6671eb26.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "127d8ece5a9af4e2422ef4ebccb5724f",
+    "url": "https://platiagro.github.io/platiagro/machine-failure/index.html"
+  },
+  {
+    "revision": "ae59da66d76ff7af0d4f",
+    "url": "https://platiagro.github.io/platiagro/machine-failure/static/css/2.42ec2a3e.chunk.css"
+  },
+  {
+    "revision": "aa86195a197ad4b07d71",
+    "url": "https://platiagro.github.io/platiagro/machine-failure/static/css/main.a5868bd7.chunk.css"
+  },
+  {
+    "revision": "ae59da66d76ff7af0d4f",
+    "url": "https://platiagro.github.io/platiagro/machine-failure/static/js/2.f1bdbd2e.chunk.js"
+  },
+  {
+    "revision": "eaaf63558baae456918a991a401208d7",
+    "url": "https://platiagro.github.io/platiagro/machine-failure/static/js/2.f1bdbd2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa86195a197ad4b07d71",
+    "url": "https://platiagro.github.io/platiagro/machine-failure/static/js/main.fa5a3476.chunk.js"
+  },
+  {
+    "revision": "6aefdad29ed4d543a838",
+    "url": "https://platiagro.github.io/platiagro/machine-failure/static/js/runtime-main.96509b1c.js"
+  }
+]);
