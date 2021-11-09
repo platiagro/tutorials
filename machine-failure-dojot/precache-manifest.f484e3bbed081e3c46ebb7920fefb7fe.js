3d8a30a7de189c6bcbe0fc38d9ca2054e347f3a2
fberanizo <fabio.beranizo@gmail.com>
2021-11-09 16:25:12 +0000

    Deploying to gh-pages from  @ 6625907db98a632d9578487aa6516249e5056dc6 🚀

---
+++ machine-failure-dojot/precache-manifest.f484e3bbed081e3c46ebb7920fefb7fe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "759c4f146f4a0b634519525831b666a8",
+    "url": "https://platiagro.github.io/tutorials/machine-failure-dojot/index.html"
+  },
+  {
+    "revision": "0d726f163c4f7c8afde2",
+    "url": "https://platiagro.github.io/tutorials/machine-failure-dojot/static/css/2.e10332d9.chunk.css"
+  },
+  {
+    "revision": "011926c7103e9d1fdcf9",
+    "url": "https://platiagro.github.io/tutorials/machine-failure-dojot/static/css/main.032a20cb.chunk.css"
+  },
+  {
+    "revision": "0d726f163c4f7c8afde2",
+    "url": "https://platiagro.github.io/tutorials/machine-failure-dojot/static/js/2.648db3f4.chunk.js"
+  },
+  {
+    "revision": "8601f73ae961fbfa1340144b4d18543b",
+    "url": "https://platiagro.github.io/tutorials/machine-failure-dojot/static/js/2.648db3f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "011926c7103e9d1fdcf9",
+    "url": "https://platiagro.github.io/tutorials/machine-failure-dojot/static/js/main.46ff1ead.chunk.js"
+  },
+  {
+    "revision": "07b01f501fdf22b09877",
+    "url": "https://platiagro.github.io/tutorials/machine-failure-dojot/static/js/runtime-main.331eb5dd.js"
+  }
+]);
